f1a7636df0040f7b72006dd96f04067a2ba3daa4
bui <thibault@crowdsec.net>
2023-05-25 22:29:10 +0200

    extend more

--- pattern.go
+++ pattern.go
@@ -1,7 +1,6 @@
 package grokky
 
 type Pattern interface {
-	FindStringSubmatch(s string) []string
 	String() string
 	Names() []string
 	Parse(input string) map[string]string
@@ -9,6 +8,10 @@ type Pattern interface {
 	//ripped from regexp.Regexp
 	Match([]byte) bool
 	FindStringIndex(string) (loc []int)
+
 	FindAllStringSubmatchIndex(s string, n int) [][]int
+	FindStringSubmatchIndex(s string) []int
+
 	FindAllStringSubmatch(s string, n int) [][]string
+	FindStringSubmatch(s string) []string
 }
